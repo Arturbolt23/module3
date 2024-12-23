740ca3053fa0956968c17a165c3a3438907903c4
Artyr <boltinovartua@gmail.com>
2024-12-23 22:15:44 +0300

    11

---
+++ module7/7-4_format.py
@@ -0,0 +1,8 @@
+team1 = 'Мастер кода'
+team2 = 'Волщебники данных'
+team1_num = 5
+team2_num = 6
+print('В команде %s учаастников - %d' % (team1, team1_num))
+score2 = 42
+print('Команда {} решила задач - {}'.format(team2, score2))
+
